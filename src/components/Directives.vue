8f2f996c4e37da45cbdece99f90187dd101a3d97
xmasuhai <xmasuhai@163.com>
2020-09-24 06:38:56 +0800

    directives

---
+++ src/components/Directives.vue
@@ -0,0 +1,28 @@
+<template>
+<div v-x v-y>
+  <p>hello</p>
+</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+
+    }
+  },
+  directives: {
+    'x': {
+      inserted: function (el) {
+        el.addEventListener('click', () => {
+          console.log('x');
+        })
+      }
+    },
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -4,6 +4,18 @@ import router from './router'
 
 Vue.config.productionTip = false
 
+// 全局指令
+
+Vue.directive('y', {
+  inserted: function (el) {
+    console.log(el)
+    // 点击即出现一个y
+    el.addEventListener('click', () => {
+      console.log('y')
+    })
+  },
+})
+
 new Vue({
   router,
   render: h => h(App)
