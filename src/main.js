10e952f98268a0b25da568286106ccb65e1da26a
xmasuhai <xmasuhai@163.com>
2021-01-05 07:22:57 +0800

    hash route

--- src/main.js
+++ src/main.js
@@ -27,47 +27,31 @@ new Vue({
 // JS Router http://localhost:8080/#1
 /* 获取用户取得的地址 */
 console.log(`-------------------`)
-console.log(window.location.hash)
-const showRouter = document.querySelector('#showRouter')
-const createDiv = () => document.createElement("div")
-const [div1, div2, div3, div4] = [createDiv(), createDiv(), createDiv(), createDiv()]
-  ;[div1.innerHTML, div2.innerHTML, div3.innerHTML, div4.innerHTML] = ["1", "2", "3", "4"]
-const routeTable = {
-  "1": div1,
-  "2": div2,
-  "3": div3,
-  "4": div4
-};
-function doRoute(container) {
-  // 获取hash地址 #1 #$ 中的数字 1 $
-  let number = window.location.hash.substr(1);
-
-  // 默认路由，当hash值为空时将它设为1
-  number = number || 1;
-
-  // 根据hash得到路由表中相应页面
-  let div = routeTable[number.toString()];
-
-  // 如果路由表中不存在则跳转到404页面
-  if (!div) {
-    div = document.querySelector("#div404");
-  }
-
-  // 渲染页面
-  div.style.display = "block";
-
-  // 先将容器中清空
-  container.innerHTML = "";
-
-  // 展示内容
-  container.appendChild(div);
-  console.log(number);
-}
-
-doRoute(showRouter)
+let hashNumber = window.location.hash.substr(1)
+// 获取界面
+let div = document.querySelector(`#div${hashNumber}`)
+let app = document.querySelector("#showRouter")
+// 渲染页面
+if (div) div.style.display = "block"
+// 展示内容
+if (app) app.appendChild(div)
 
 /* 监听hash变化 */
 window.addEventListener('hashchange', () => {
-  doRoute(showRouter)
-})
+  console.log("hash变了")
 
+  const hashNumber2 = window.location.hash.substr(1)
+
+  // 获取界面
+  const div2 = document.querySelector(`#div${hashNumber2}`)
+  const app2 = document.querySelector("#showRouter")
+
+  // 渲染页面
+  div2.style.display = "block"
+  // 先将容器中清空
+  console.log(app2);
+  app2.children[0].style.display = "none"
+  // 展示内容
+  document.body.appendChild(app2.children[0])
+  app2.appendChild(div2)
+})
